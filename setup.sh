b77b231a6fdc16fab93df53cbe2efd392936afc5
ken-10 <46728332+ken-10@users.noreply.github.com>
2024-01-03 21:43:08 -1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function up {
+function start {
     docker volume prune -f
     docker build -t poketcgdata-airflow -f ./Dockerfile ./
     docker-compose -f docker-compose.yaml up --remove-orphans -d
