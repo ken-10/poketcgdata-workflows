5b8705d88bf0d74b4fddadeced44a9cfef3aef0e
Ken Tung <kentung@hawaii.edu>
2024-01-21 19:17:44 -1000

    Create rest_api_client.py and unit tests

---
+++ tests/unit/test_rest_api_client.py
@@ -0,0 +1,17 @@
+import pytest
+from common_packages.rest_api_client import RestApiClient
+
+
+@pytest.mark.parametrize("input_params", [({"api_key": "API_KEY", "api_url": "API_URL", "timeout_in_seconds": 10}),
+                                          ({"api_key": "API_KEY", "api_url": "API_URL"})])
+def test_rest_api_client_initialization(input_params):
+    expected_attributes = input_params
+    if "timeout_in_seconds" not in input_params:
+        rest_client = RestApiClient(api_url=input_params["api_url"], api_key=input_params["api_key"])
+        expected_attributes["timeout_in_seconds"] = 30
+    else:
+        rest_client = RestApiClient(api_url=input_params["api_url"], api_key=input_params["api_key"],
+                                    timeout_in_seconds=input_params["timeout_in_seconds"])
+
+    assert rest_client.__dict__ == expected_attributes
+    
