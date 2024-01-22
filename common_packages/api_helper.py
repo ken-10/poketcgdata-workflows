1a9a29606ce5a94ca605d354861dd142c0a132ac
Ken Tung <kentung@hawaii.edu>
2024-01-21 21:57:31 -1000

    Added edge case to get_all for resource types that are not Card nor Set
    Resource types that are not Card or Set do not have pages. Adding pages as query params causes an infinite loop.

--- common_packages/api_helper.py
+++ common_packages/api_helper.py
@@ -1,24 +1,32 @@
 import logging
 import requests
+from enums.APIResource import APIResource
 
 
-def get_all(api_url: str, api_key: str, resource: str, timeout_in_seconds: float = 30):
+def get_all(api_url: str, api_key: str, resource: APIResource, timeout_in_seconds: float = 30):
     """
     Visit following link for supported resources - https://docs.pokemontcg.io/
     """
     params = {'page': 1}
     results = []
-    logging.info(f"Getting all {resource}")
+    logging.info(f"Getting all {resource.value}")
+    retrieved_count = 0
     while True:
-        url = f'{api_url}/{resource}'
+        url = f'{api_url}/{resource.value}'
         rest_client = RestApiClient(url, api_key, timeout_in_seconds)
         response = rest_client.get(params).json()
         response_data = response['data']
         if len(response_data) <= 0:
             break
         results.extend(response_data)
-        logging.info(f"Retrieved {response['count']} out of {response['totalCount']}.")
-        params['page'] += 1
+        retrieved_count += len(response_data)
+        logging.info(f'Retrieved {retrieved_count} {resource.value}')
+
+        # Types, subtypes, supertypes, and rarities do not have pages
+        if 'page' in response:
+            params['page'] += 1
+        else:
+            break
     return results
 
 
