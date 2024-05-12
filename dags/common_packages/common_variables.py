d247cdd6534baa5fcf8115d582b18ee7397c9217
ken-10 <46728332+ken-10@users.noreply.github.com>
2024-05-11 16:38:01 -1000

    Update common_variables.py with dbt paths

--- dags/common_packages/common_variables.py
+++ dags/common_packages/common_variables.py
@@ -2,5 +2,11 @@ api_url = 'https://api.pokemontcg.io/v2'
 
 airflow_paths = {
     'sql_queries': '/opt/airflow/common_packages/resources/sql',
-    'dags_data': '/opt/airflow/dags_data'
+    'dags_data': '/opt/airflow/dags_data',
+    'dbt': {
+        'base': '/opt/airflow/dags/dbt',
+        'poketcgdata': '/opt/airflow/dags/dbt/poketcgdata'
+    }
 }
+
+dbt_executable_path = '/opt/airflow/dbt_venv/bin/dbt'
