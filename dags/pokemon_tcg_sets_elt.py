4c99987f2194cdae6df7555c4fe567b93ed401f3
ken-10 <46728332+ken-10@users.noreply.github.com>
2024-05-11 16:38:42 -1000

    Combine extract load and transform DAG into one pokemon_tcg_sets_elt.py DAG

--- dags/pokemon_tcg_sets_elt.py
+++ dags/pokemon_tcg_sets_elt.py
@@ -2,17 +2,34 @@ import os.path
 import pendulum
 
 from airflow.decorators import dag, task
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from common_packages import config_helper, json_helper
 from common_packages import api_helper, common_variables, file_helper, postgresql_helper
 from common_packages.enums.APIResource import APIResource
+from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig, ExecutionConfig
+from cosmos.profiles import PostgresUserPasswordProfileMapping
 
 _AIRFLOW_PATHS = common_variables.airflow_paths
 _API_URL = common_variables.api_url
+_DBT_EXECUTABLE_PATH = common_variables.dbt_executable_path
+
+profile_config = ProfileConfig(
+    profile_name="poketcgdata",
+    target_name="dev",
+    profile_mapping=PostgresUserPasswordProfileMapping(
+        conn_id="poketcgdata_dev",
+        profile_args={"schema": "cards"},
+    ),
+)
+
+dbt_execution_config = ExecutionConfig(
+    dbt_executable_path=_DBT_EXECUTABLE_PATH,
+)
+
 
 def failure_cleanup(context):
     processing_folder = context["ti"].xcom_pull(dag_id=context["dag_run"].dag_id, task_ids="get_sets_from_api")["processing_folder"]
     file_helper.delete_folder(processing_folder)
+
 
 @dag(
     schedule=None,
@@ -20,7 +37,7 @@ def failure_cleanup(context):
     catchup=False,
     tags=["ELT", "POKEMON", "SET"],
 )
-def pokemon_tcg_sets_extract_load():
+def pokemon_tcg_sets_elt():
     @task(on_failure_callback=failure_cleanup)
     def get_sets_from_api():
         processing_folder = file_helper.create_folder(_AIRFLOW_PATHS["dags_data"], "pokemon_tcg_sets_load")
@@ -31,7 +48,7 @@ def pokemon_tcg_sets_extract_load():
         return {"processing_folder": processing_folder, "sets_csv": sets_csv}
 
     @task(on_failure_callback=failure_cleanup)
-    def truncate_landing_table(**kwargs):
+    def truncate_landing_table():
         postgresql_helper.execute_query('poketcgdata_dev', 'TRUNCATE TABLE cards.card_set_ldg;')
 
     @task(on_failure_callback=failure_cleanup)
@@ -45,11 +62,20 @@ def pokemon_tcg_sets_extract_load():
             "processing_folder"]
         file_helper.delete_folder(processing_folder)
 
-    trigger_dbt_transform_dag = TriggerDagRunOperator(
-        task_id='trigger_dbt_transform_dag',
-        trigger_dag_id='pokemon_tcg_sets_dbt_transform'
+    dbt_transfrom = DbtTaskGroup(
+        group_id="transform_data",
+        project_config=ProjectConfig(os.path.join(_AIRFLOW_PATHS["dbt"]["poketcgdata"], "card_sets")),
+        profile_config=profile_config,
+        execution_config=dbt_execution_config,
     )
 
-    get_sets_from_api() >> truncate_landing_table() >> bulk_copy_to_landing_table() >> remove_folder_and_contents() >> trigger_dbt_transform_dag
+    (
+            get_sets_from_api()
+            >> truncate_landing_table()
+            >> bulk_copy_to_landing_table()
+            >> remove_folder_and_contents()
+            >> dbt_transfrom
+    )
 
-pokemon_tcg_sets_extract_load()
+
+pokemon_tcg_sets_elt()
