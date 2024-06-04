33e291cb528a468426d17cc952fd936a8b878c24
Ken Tung <kentung@hawaii.edu>
2024-06-03 17:25:18 -1000

    Modify airflow executor from celery to local and removed unused components

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
-
+#!/bin/sh
 function start {
     docker volume prune -f
     docker build -t poketcgdata-airflow -f ./Dockerfile ./
-    docker-compose -f docker-compose.yaml up --remove-orphans -d
+    docker compose -f docker-compose.yaml up --remove-orphans -d
 }
 
 function down {
-    docker-compose down
+    docker compose down
 }
 
 function restart {
