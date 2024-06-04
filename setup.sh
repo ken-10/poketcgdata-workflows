1269b4b2a36358732fed7a7c29bc4ec43d8da9b1
Ken Tung <kentung@hawaii.edu>
2024-06-03 16:47:55 -1000

    replace "docker-compose" with "docker compose" in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,11 @@
 function start {
     docker volume prune -f
     docker build -t poketcgdata-airflow -f ./Dockerfile ./
-    docker-compose -f docker-compose.yaml up --remove-orphans -d
+    docker compose -f docker-compose.yaml up --remove-orphans -d
 }
 
 function down {
-    docker-compose down
+    docker compose down
 }
 
 function restart {
