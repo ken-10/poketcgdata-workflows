c2d4b980c7836c0f29f163916e5ba793919435cf
Ken Tung <kentung@hawaii.edu>
2024-06-03 16:45:42 -1000

    Update mssql_helper.py
    Add execute_query() and bcp_file_to_table()

--- dags/common_packages/mssql_helper.py
+++ dags/common_packages/mssql_helper.py
@@ -1,9 +1,36 @@
+import logging
+import subprocess
 from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook
 
 
-def bulk_copy(conn_id: str, table: str, elements_to_insert: list, column_ids: list = None, tablock: bool = False,
-              batch_size: int = 1000):
+def execute_query(conn_id: str, sql_query: str, params=None):
     hook = MsSqlHook(mssql_conn_id=conn_id)
     with hook.get_conn() as conn:
-        conn.bulk_copy(table_name=table, elements=elements_to_insert, column_ids=column_ids, batch_size=batch_size,
-                       tablock=tablock)
+        with conn.cursor() as csr:
+            if not params:
+                params = ()
+            logging.info(f"Executing given query with params: {params}")
+            csr.execute(sql_query, params)
+            conn.commit()
+            logging.info(f"Executed query")
+
+
+def bcp_file_to_table(database: str, schema: str, table: str, file_path: str, server: str, username: str, password: str,
+                      field_terminator=None, char_data_type=None, row_terminator=None):
+    # TODO: Modify function to get attributes from airflow connection
+    logging.info(f'Attempting to bulk copy {file_path} to {database}.{schema}.{table}')
+    cmd_prefix = f'bcp {database}.{schema}.{table} in {file_path}'
+    cmd_suffix = f'-S {server} -U {username} -P {password}'
+    cmd_options = '-c'
+    if char_data_type:
+        cmd_options += f' {char_data_type}'
+    if field_terminator:
+        cmd_options += f' -t{field_terminator}'
+    if row_terminator:
+        cmd_options += f' -r {row_terminator}'
+
+    final_cmd = f"{cmd_prefix} {cmd_options} {cmd_suffix}"
+    subprocess.run(final_cmd, shell=True)
+    logging.info(f'Successfully bulk copied {file_path} to {database}.{schema}.{table}')
+
+    return final_cmd
